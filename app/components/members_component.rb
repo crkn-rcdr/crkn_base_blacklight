3a7e0000cf8dd733785980ad3159f407830676c7
Brittny Lapierre <brittny.lapierre@gmail.com>
2026-02-20 10:29:04 -0500

    Full member logos

--- app/components/members_component.rb
+++ app/components/members_component.rb
@@ -1,6 +1,89 @@
 class MembersComponent < ViewComponent::Base
   require 'zlib'
 
+  LOGO_ASSETS = {
+    'Acadia University' => 'members/acadia-university.png',
+    'Algoma University' => 'members/algoma-university.png',
+    'Athabasca University' => 'members/athabasca-university.png',
+    'Bibliotheque et Archives nationales du Quebec' => 'members/bibliotheque-et-archives-nationales-du-quebec.png',
+    "Bishop's University" => 'members/bishop-s-university.png',
+    'Brandon University' => 'members/brandon-university.png',
+    'Brock University' => 'members/brock-university.png',
+    'Cape Breton University' => 'members/cape-breton-university.png',
+    'Capilano University' => 'members/capilano-university.png',
+    'Carleton University' => 'members/carleton-university.png',
+    'Concordia University' => 'members/concordia-university.png',
+    'Concordia University of Edmonton' => 'members/concordia-university-of-edmonton.png',
+    'Dalhousie University' => 'members/dalhousie-university.png',
+    'Ecole Polytechnique de Montreal' => 'members/ecole-polytechnique-de-montreal.png',
+    'Ecole de technologie superieure (UQ)' => 'members/ecole-de-technologie-superieure-uq.png',
+    "Ecole nationale d'administration publique (UQ)" => 'members/ecole-nationale-d-administration-publique-uq.png',
+    'HEC Montreal' => 'members/hec-montreal.png',
+    'Institut national de la recherche scientifique (UQ)' => 'members/institut-national-de-la-recherche-scientifique-uq.png',
+    'Kwantlen Polytechnic University' => 'members/kwantlen-polytechnic-university.png',
+    'Lakehead University' => 'members/lakehead-university.png',
+    'Laurentian University' => 'members/laurentian-university.png',
+    'Library and Archives Canada' => 'members/library-and-archives-canada.png',
+    'MacEwan University' => 'members/macewan-university.png',
+    'McGill University' => 'members/mcgill-university.png',
+    'McMaster University' => 'members/mcmaster-university.png',
+    'Memorial University of Newfoundland' => 'members/memorial-university-of-newfoundland.png',
+    'Mount Allison University' => 'members/mount-allison-university.png',
+    'Mount Royal University' => 'members/mount-royal-university.png',
+    'Mount Saint Vincent University' => 'members/mount-saint-vincent-university.png',
+    'Nipissing University' => 'members/nipissing-university.png',
+    'Nova Scotia College of Art and Design University' => 'members/nova-scotia-college-of-art-and-design-university.png',
+    'OCAD University' => 'members/ocad-university.png',
+    'Ontario Tech University' => 'members/ontario-tech-university.png',
+    "Queen's University" => 'members/queen-s-university.png',
+    'Royal Military College of Canada' => 'members/royal-military-college-of-canada.png',
+    'Royal Roads University' => 'members/royal-roads-university.png',
+    "Saint Mary's University" => 'members/saint-mary-s-university.png',
+    'Simon Fraser University' => 'members/simon-fraser-university.png',
+    'St. Francis Xavier University' => 'members/st-francis-xavier-university.png',
+    'Tele-universite du Quebec' => 'members/tele-universite-du-quebec.png',
+    "The King's University" => 'members/the-king-s-university.png',
+    'Thompson Rivers University' => 'members/thompson-rivers-university.png',
+    'Toronto Metropolitan University' => 'members/toronto-metropolitan-university.png',
+    'Toronto Public Library' => 'members/toronto-public-library.png',
+    'Trent University' => 'members/trent-university.png',
+    'Trinity Western University' => 'members/trinity-western-university.png',
+    'Universite Laval' => 'members/universite-laval.png',
+    'Universite Sainte-Anne' => 'members/universite-sainte-anne.png',
+    'Universite de Moncton' => 'members/universite-de-moncton.png',
+    'Universite de Montreal' => 'members/universite-de-montreal.png',
+    'Universite de Sherbrooke' => 'members/universite-de-sherbrooke.png',
+    'Universite du Quebec a Chicoutimi' => 'members/universite-du-quebec-a-chicoutimi.png',
+    'Universite du Quebec a Montreal' => 'members/universite-du-quebec-a-montreal.png',
+    'Universite du Quebec a Rimouski' => 'members/universite-du-quebec-a-rimouski.png',
+    'Universite du Quebec a Trois-Rivieres' => 'members/universite-du-quebec-a-trois-rivieres.png',
+    'Universite du Quebec en Abitibi-Temiscamingue' => 'members/universite-du-quebec-en-abitibi-temiscamingue.png',
+    'Universite du Quebec en Outaouais' => 'members/universite-du-quebec-en-outaouais.png',
+    'University of Alberta' => 'members/university-of-alberta.png',
+    'University of British Columbia' => 'members/university-of-british-columbia.png',
+    'University of Calgary' => 'members/university-of-calgary.png',
+    'University of Guelph' => 'members/university-of-guelph.png',
+    'University of Lethbridge' => 'members/university-of-lethbridge.png',
+    'University of Manitoba' => 'members/university-of-manitoba.png',
+    'University of New Brunswick' => 'members/university-of-new-brunswick.png',
+    'University of Northern British Columbia' => 'members/university-of-northern-british-columbia.png',
+    'University of Ottawa' => 'members/university-of-ottawa.png',
+    'University of Prince Edward Island' => 'members/university-of-prince-edward-island.png',
+    'University of Regina' => 'members/university-of-regina.png',
+    'University of Saskatchewan' => 'members/university-of-saskatchewan.png',
+    'University of Toronto' => 'members/university-of-toronto.png',
+    'University of Victoria' => 'members/university-of-victoria.png',
+    'University of Waterloo' => 'members/university-of-waterloo.png',
+    'University of Windsor' => 'members/university-of-windsor.png',
+    'University of Winnipeg' => 'members/university-of-winnipeg.png',
+    'University of the Fraser Valley' => 'members/university-of-the-fraser-valley.png',
+    'Vancouver Island University' => 'members/vancouver-island-university.png',
+    'Western University' => 'members/western-university.png',
+    'Wilfrid Laurier University' => 'members/wilfrid-laurier-university.png',
+    'York University' => 'members/york-university.png',
+    'Yukon University' => 'members/yukon-university.png'
+  }.freeze
+
   LOGO_WORDMARKS = {
     'University of Victoria' => 'UVIC',
     'University of Ottawa' => 'uOttawa',
@@ -73,6 +156,10 @@ class MembersComponent < ViewComponent::Base
 
   def search_key(name)
     normalize_name(name).downcase
+  end
+
+  def logo_asset(name)
+    LOGO_ASSETS[normalize_name(name)]
   end
 
   private
