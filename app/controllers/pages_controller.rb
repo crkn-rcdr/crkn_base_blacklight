f6c5b8e0923b7b266ee768f6516fe4f1a6035e2f
Brittny Lapierre <brittny.lapierre@gmail.com>
2026-04-09 10:38:28 -0400

    Updating base layout (#23)
    * Saving image deploy
    
    * Fix for some build errors on azure
    
    * Fixes for usability test 1
    
    * WCAG 2.2 AAA
    
    * Saving accessibility changes
    
    * Save cleaned bgs
    
    * Clean BG
    
    * Fix bgs
    
    * Update sizing
    
    * Modernize hero
    
    * Stacked cards canadiana
    
    * Backgrounds for heritage and canadiana
    
    * Update canadiana.scss
    
    * WCAG AAA
    
    * Adding jump button
    
    * Fix members bug
    
    * Mobile engancements
    
    * Saving page transfer progress
    
    * Correcting french chars
    
    * headers for about pages
    
    * Compare page
    
    * Baseline for all pages
    
    * Base layouts ready
    
    * More edits to page settings
    
    * A few more fixes
    
    * Correcting case
    
    * Update _user_util_links.html.erb
    
    * Update home.fr.html.erb
    
    * Update home.fr.html.erb
    
    * Update _user_util_links.html.erb
    
    * Update _user_util_links.html.erb
    
    * more static page fixes
    
    * Adding text for about pages
    
    * Update production.rb
    
    * Adding env var rence to dev
    
    * More content updates
    
    * About page design
    
    * Proper navigation
    
    * What is iiif page style
    
    * iSaving the header for the search
    
    * Saving styling
    
    * Adding a nice scrolling library for exhibits
    
    * Good exhibit sizing and scroll behaviour
    
    * Use lenis for animations throughout
    
    * Perfect alignment on heritage
    
    * Good height on large screens
    
    * isearch results
    
    * Header for canadiana search image ratios correct
    
    * Correct links
    
    * Some fixes on the canadiana about page
    
    * IFlex layout cardsx
    
    * Non relative card positions
    
    * differen animaton canadiana about large screen
    
    * Fixes for styling issues
    
    * Updated exhibit manifest url
    
    * Simplify headers
    
    * Mirador thumbnail windows size change fix
    
    * Marc formatting docs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,15 @@ class PagesController < ApplicationController
     def home
     end
 
-    def about_monographs; end
-    def about_serials; end
-    def about_govpubs; end
-    def about_maps; end
+    def compare_collections; end
+    def about_canadiana; end
+    def about_heritage; end
+    def api_access; end
     def what_is_iiif; end
+    def citation_guide; end
+    def navigating_collections; end
+    def contact_us; end
+    def system_status; end
+    def sitemap; end
+    def terms_of_service; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
   concern :range_searchable, BlacklightRangeLimit::Routes::RangeSearchable.new
+  get '/catalog/:id.txt', to: 'catalog#tx_gen', as: :catalog_tx_gen, constraints: { id: /[^\/]+/ }, defaults: { format: :txt }
   mount Blacklight::Engine => '/'
   root to: "pages#home"
   # Simple about pages for collections
-  get '/about/monographs', to: 'pages#about_monographs'
-  get '/about/serials', to: 'pages#about_serials'
-  get '/about/government-publications', to: 'pages#about_govpubs'
-  get '/about/maps', to: 'pages#about_maps'
+  get '/compare-collections', to: 'pages#compare_collections', as: :compare_collections
+  get '/about/canadiana', to: 'pages#about_canadiana', as: :about_canadiana
+  get '/about/heritage', to: 'pages#about_heritage', as: :about_heritage
+  get '/api-access', to: 'pages#api_access', as: :api_access
   get '/what-is-iiif', to: 'pages#what_is_iiif', as: :what_is_iiif
+  get '/citation-guide', to: 'pages#citation_guide', as: :citation_guide
+  get '/navigating-collections', to: 'pages#navigating_collections', as: :navigating_collections
+  get '/contact-us', to: 'pages#contact_us', as: :contact_us_page
+  get '/system-status', to: 'pages#system_status', as: :system_status
+  get '/sitemap', to: 'pages#sitemap', as: :sitemap
+  get '/terms-of-service', to: 'pages#terms_of_service', as: :terms_of_service
   # Allow slashes inside :ark (e.g., ark:/69429/m0k35m90313z)
   get '/dl/:id/*ark', to: 'downloads#index', constraints: { id: /[0-z\.]+/ }, format: false
   #root to: "catalog#index"
